a176f7adcf3bfbb1efaf7b59a325c256fdf79003
Kyle Steinfeld <ksteinfe@gmail.com>
2024-04-23 14:04:42 -0700

    small

--- game-0/src/app/page.tsx
+++ game-0/src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
     };
 
     return (
-      <div>
+      <div className="page-container">
           <div className="form-container">
               <input
                   type="text"
